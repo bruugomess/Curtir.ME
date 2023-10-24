258814347743804ae0d6f50187491a6e6e766f9b
NataliaMacedoBaiao <148367291+NataliaMacedoBaiao@users.noreply.github.com>
2023-10-23 23:06:48 -0300

    Implementação da Classe Usuario
    Adição de Getters e Setters na classe, Criação de usuario e limpeza de usuario

--- Main.cpp
+++ Main.cpp
@@ -6,22 +6,18 @@
 
 #include "Usuario.h"
 
-#define tamanhoNome 50
-#define tamanhoSenha 50
 #define sucesso 0
-Usuario usuarioAtual;
 
+Usuario usuarioAtual;
 using namespace std;
 
 /*Declaração das Funções*/
+bool autenticarUsuario(char nome[], char senha[]);
 void printaMenuLogin();
 void printaMenu();
-
-bool autenticarUsuario();
-void cadastrarUsuario();
-
+void limpaEntrada();
+void cadastrarUsuario(char nome[], char senha[]);
 /*Declaração das Funções*/
-
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
@@ -30,20 +26,33 @@ int main() {
     do {
 
         /*Se não existe um usuario*/
-        if(!strcmp(usuarioAtual.senha, "") != 0){
+        if(!strcmp(usuarioAtual.getSenha(), "") != 0){
             //printf("Usuario Atual: %s", usuarioAtual.nome);
             printaMenuLogin();
 
             cin >> opcao;
+            char nome[tamanhoNome], senha[tamanhoSenha];
             switch (opcao) {
             case 1:
-                cadastrarUsuario();
+                cout << "   Digite o nome do usuário: ";
+                limpaEntrada();
+                fgets(nome, tamanhoNome, stdin);
+
+                cout << "   Digite a senha do usuário: ";
+                fgets(senha, tamanhoSenha, stdin);
+                cadastrarUsuario(nome, senha);
                 break;
             case 2:
-                if (autenticarUsuario()) {
-                    cout << "Autenticação bem-sucedida.\n";
+                cout << "   Nome: ";
+                limpaEntrada();
+                fgets(nome, tamanhoNome,stdin);
+                cout << "   Senha: ";
+                fgets(senha, tamanhoSenha, stdin);
+
+                if (autenticarUsuario(nome,senha)) {
+                    cout << "   Autenticação bem-sucedida.\n";
                 }else{
-                    cout << "Usuario ou senha incorretos.\n";
+                    cout << "   Usuario ou senha incorretos.\n";
                     system("pause");
                 }
                 break;
@@ -53,6 +62,18 @@ int main() {
         }else{ //Se existe um usuario
             printaMenu();
             cin >> opcao;
+
+            switch(opcao){
+                case 3:
+                    break;
+                case 2:
+                    break;
+                case 1:
+                    break;
+                case 0:
+                    usuarioAtual.limpaUsuario();
+                    break;
+            }
         }
     } while (1);
 
@@ -60,48 +81,56 @@ int main() {
 }
 
 
-void cadastrarUsuario() {
-    ofstream arquivo("Arquivos/usuarios.bin", ios::binary | ios::app);
+void printaMenuLogin(){
+        system("cls");
+        cout <<"    .....................................................................\n";
+        cout <<"    .                                                                   .\n";
+        cout <<"    .                            Login                                  .\n";
+        cout <<"    .                                                                   .\n";
+        cout <<"    .....................................................................\n";
 
-    if (!arquivo.is_open()) {
-        cout << "Erro ao abrir o arquivo de usuários." << endl;
-        return;
-    }
-
-    Usuario usuario;
-
-    cout << "Digite o nome do usuário: ";
-    gets(usuario.nome);
-    fgets(usuario.nome, tamanhoNome, stdin);
-
-    cout << "Digite a senha do usuário: ";
-    fgets(usuario.senha, tamanhoSenha, stdin);
-
-    arquivo.write((char*)&usuario, sizeof(Usuario));
-    arquivo.close();
-
-    cout << "Usuário cadastrado com sucesso." << endl;
+        cout <<"    1 - Cadastrar usuário\n";
+        cout <<"    2 - Entrar\n";
+        cout <<"    0 - Sair\n";
+        cout <<"    Digite a opção: ";
 }
 
-bool autenticarUsuario() {
+void printaMenu(){
+        system("cls");
+        cout <<"    .....................................................................\n";
+        cout <<"    .                                                                   .\n";
+        cout <<"    .                             Menu                                  .\n";
+        cout <<"    .                                                                   .\n";
+        cout <<"    .....................................................................\n";
+
+
+        printf("\n\n    Perfil: %s\n", usuarioAtual.getNome());
+
+        cout <<"    1 - Feed\n";
+        cout <<"    2 - Postagens\n";
+        cout <<"    3 - Seguir / Não seguir\n";
+        cout <<"    0 - Sair\n";
+        cout <<"    Digite a opção: ";
+}
+
+void limpaEntrada(){
+    char lixo[2];
+    gets(lixo);
+}
+
+bool autenticarUsuario(char nome[], char senha[]) {
 
     ifstream arquivo("Arquivos/usuarios.bin", ios::binary);
     Usuario usuario;
-    char nome[tamanhoNome], senha[tamanhoSenha];
 
     if (!arquivo.is_open()) {
         cout << "Erro ao abrir o arquivo de usuários." << endl;
         return false;
     }else{
-        cout << "Nome: ";
-        gets(nome);
-        fgets(nome, tamanhoNome,stdin);
-        cout << "Senha: ";
-        fgets(senha, tamanhoSenha, stdin);
 
         while (arquivo.read((char*)&usuario, sizeof(Usuario))) {
 
-            if (strcmp(usuario.nome,nome) == 0 && strcmp(usuario.senha,senha) == 0) {
+            if (strcmp(usuario.getNome(),nome) == 0 && strcmp(usuario.getSenha(),senha) == 0) {
                 usuarioAtual = usuario;
                 arquivo.close();
                 return true;
@@ -114,35 +143,21 @@ bool autenticarUsuario() {
     return false;
 }
 
-void printaMenuLogin(){
-        system("cls");
-        cout <<".....................................................................\n";
-        cout <<".                                                                   .\n";
-        cout <<".                            Login                                  .\n";
-        cout <<".                                                                   .\n";
-        cout <<".....................................................................\n";
+void cadastrarUsuario(char nome[], char senha[]) {
+    ofstream arquivo("Arquivos/usuarios.bin", ios::binary | ios::app);
 
-        cout <<"1 - Cadastrar usuário\n";
-        cout <<"2 - Entrar\n";
-        cout <<"0 - Sair\n";
-        cout <<"Digite a opção: \n";
+    if (!arquivo.is_open()) {
+        cout << "Erro ao abrir o arquivo de usuários." << endl;
+        return;
+    }
+
+    Usuario usuario;
+
+    usuario.setNome(nome); //Define o nome para o usuario
+    usuario.setSenha(senha);
+
+    arquivo.write((char*)&usuario, sizeof(Usuario));
+    arquivo.close();
+
+    cout << "Usuário cadastrado com sucesso." << endl;
 }
-
-void printaMenu(){
-        system("cls");
-        cout <<".....................................................................\n";
-        cout <<".                                                                   .\n";
-        cout <<".                             Menu                                  .\n";
-        cout <<".                                                                   .\n";
-        cout <<".....................................................................\n";
-
-
-        printf("\n\nPerfil: %s\n", usuarioAtual.nome);
-
-        cout <<"1 - Feed\n";
-        cout <<"2 - Postagens\n";
-        cout <<"2 - Seguir / Não seguir\n";
-        cout <<"0 - Sair\n";
-        cout <<"Digite a opção: \n";
-}
-
