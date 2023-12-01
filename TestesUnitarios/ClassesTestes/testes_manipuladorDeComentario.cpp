a422bde71937c645a3ab0c84b9c7fcb2c0422a60
PedroBelloSoares <148364534+PedroBelloSoares@users.noreply.github.com>
2023-12-01 16:58:35 -0300

    Delete TestesUnitarios directory

--- TestesUnitarios/ClassesTestes/testes_manipuladorDeComentario.cpp
+++
@@ -1,9 +0,0 @@
-#include "testes_manipuladorDeComentario.h"
-
-
-
-void testes_manipuladorDeComentario::executaTestes(){
-    manipuladorDeComentario manipuladorComentarios;
-    Comentario coment(1,"teste",1,1,"Comentario Teste");
-    CHECK(manipuladorComentarios.adicionaAoArquivo(coment) == true);
-}
--- TestesUnitarios/ClassesTestes/testes_postagem.cpp
+++
@@ -1,9 +0,0 @@
-#include "testes_postagem.h"
-
-void testes_postagem::executaTestes(){
-    Postagem postTeste(1,1,"Texto teste");
-    CHECK(postTeste.existe() == true);
-    CHECK(postTeste.GetnumeroPostagem() == 1);
-    CHECK(postTeste.GetIDusuario() == 1);
-    CHECK(strcmp(postTeste.Getconteudo(), "Texto teste") == 0);
-}
