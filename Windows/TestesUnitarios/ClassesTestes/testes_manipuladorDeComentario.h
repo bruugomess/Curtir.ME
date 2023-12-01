59481bcd09b4aae437e3d772529e380324b08afd
NataliaMacedoBaiao <148367291+NataliaMacedoBaiao@users.noreply.github.com>
2023-12-01 16:55:24 -0300

    Projeto otimizado para Windows
    Como o projeto já era baseado para Windows só separei tudo em uma pasta

---
+++ Windows/TestesUnitarios/ClassesTestes/testes_manipuladorDeComentario.h
@@ -0,0 +1,15 @@
+#ifndef TESTES_MANIPULADORDECOMENTARIO_H
+#define TESTES_MANIPULADORDECOMENTARIO_H
+
+
+class testes_manipuladorDeComentario: public Base_Teste
+{
+    public:
+        void executaTestes();
+        testes_manipuladorDeComentario(){};
+    protected:
+
+    private:
+};
+
+#endif // TESTES_MANIPULADORDECOMENTARIO_H
---
+++ Windows/TestesUnitarios/ClassesTestes/testes_usuario.h
@@ -0,0 +1,16 @@
+#ifndef TESTES_USUARIO_H
+#define TESTES_USUARIO_H
+
+#include "Base_Teste.h"
+
+
+class testes_usuario : public Base_Teste
+{
+    public:
+        testes_usuario(){};
+        void executaTestes();
+    private:
+};
+
+
+#endif // TESTES_USUARIO_H
